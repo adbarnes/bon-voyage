b11fe26a40be8d72a7d72c00fcaf391093751f28
Aaron Barnes <adbarnes@gmail.com>
2020-10-08 11:25:12 +0100

    Added fixed css and scroll functionality

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "gatsby"
 import menuStyles from './menu.module.css'
-import menuIcon from '../../src/images/menu.svg'
 
 const ListLink = props => (
     <li>
@@ -9,22 +8,73 @@ const ListLink = props => (
     </li>
   )
 
-function header(props) {
+class header extends Component {
+  state ={className: menuStyles.headerWrapper };
+  
+  componentDidMount(){
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  
+  handleScroll=()=>{
+    if (window.pageYOffset > 0) {
+        if(this.state.className === menuStyles.headerWrapper){
+          this.setState({ className: menuStyles.headerWrapperScroll });   
+        }
+    }else{
+        if(this.state.className){
+          this.setState({ className: menuStyles.headerWrapper });
+        }
+    }
+   
+  }
+  render() {
     return (
-        <header>
-        <Link className={menuStyles.brand} to="/">
+      <div ref={(r)=>this.ref=r} className={this.state.className}>
+      <header >
+         <Link className={menuStyles.brand} to="/">
           Bon voyage trips 
-        </Link>
-          <ul className={menuStyles.menu}>
-            <ListLink to="/">HOME</ListLink>
-            <ListLink to="/about/">ABOUT</ListLink>
-            <ListLink to="/contact/">CONTACT</ListLink>
-            <ListLink to="/contact/">BLOG</ListLink>
-            <ListLink to="/contact/">BOOK NOW</ListLink>
-          </ul>
-          <Link to="#" className={menuStyles.mobileMenu}><img className={menuStyles.menuIcon} src={menuIcon} /></Link>
-      </header>
+         </Link>
+       
+           <ul className={menuStyles.menu}>
+             <ListLink to="/">HOME</ListLink>
+             <ListLink to="/about/">ABOUT</ListLink>
+             <ListLink to="/contact/">CONTACT</ListLink>
+             <ListLink to="/contact/">BLOG</ListLink>
+             <ListLink to="/contact/">BOOK NOW</ListLink>
+           </ul>
+       </header>
+       </div>
     );
+  }
 }
 
 export default header;
+// import React from 'react';
+// import { Link } from "gatsby"
+// import menuStyles from './menu.module.css'
+
+// const ListLink = props => (
+//     <li>
+//       <Link to={props.to}>{props.children}</Link>
+//     </li>
+//   )
+
+// function header(props) {
+//     return (
+//         <header>
+//         <Link className={menuStyles.brand} to="/">
+//           Bon voyage trips 
+//         </Link>
+       
+//           <ul className={menuStyles.menu}>
+//             <ListLink to="/">HOME</ListLink>
+//             <ListLink to="/about/">ABOUT</ListLink>
+//             <ListLink to="/contact/">CONTACT</ListLink>
+//             <ListLink to="/contact/">BLOG</ListLink>
+//             <ListLink to="/contact/">BOOK NOW</ListLink>
+//           </ul>
+//       </header>
+//     );
+// }
+
+// export default header;
