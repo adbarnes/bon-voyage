d0e66d684438fe4402e00b27d091057628027605
Aaron Barnes <adbarnes@gmail.com>
2020-10-08 11:24:27 +0100

    Added gatsby-backgroound-image and drawer

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,22 +2,57 @@ import React from "react"
 import layoutStyles from './layout.module.css'
 import Hero from './hero'
 import Menu from './menu'
+import Drawer from '../components/drawer'
+import BackgroundImage from 'gatsby-background-image'
+import { useStaticQuery, graphql } from 'gatsby'
 
 
-export default function Layout({ children }) {
-  
+export default function Layout( { children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        indexImage: file(relativePath: { eq: "header-bg.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+              ...GatsbyImageSharpFluidLimitPresentationSize
+            }
+          }
+        }
+      }
+    `
+  )
   return (
     <>
-    <div className={layoutStyles.headerContainer}>
+    <Drawer right />
+    <BackgroundImage
+      style={{backgroundSize: 'cover'}}
+      className={layoutStyles.headerContainer}
+      fluid={data.indexImage.childImageSharp.fluid}>
+    
       <Menu />
       <Hero />
-    </div>
+   
+    </BackgroundImage>
     <div>
     {children}
     </div>
     </>
   )
 }
+
+// export const pageQuery = 
+//   graphql`
+//     query {
+//       indexImage: file(relativePath: { eq: "header-bg.jpg" }) {
+//         childImageSharp {
+//           fluid(maxWidth: 754) {
+//             ...GatsbyImageSharpFluid
+//           }
+//         }
+//       }
+//     }
+//   `
 
 
 
