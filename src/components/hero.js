e11d066fddc1c0764f8e332b8cb31a7769eb14ce
Aaron Barnes <adbarnes@gmail.com>
2020-10-08 11:30:24 +0100

    Added react-spring animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,38 +1,56 @@
-import React from 'react';
-import heroStyles from './hero.module.css'
-import bottomCurve from '../../src/images/curve-bottom.svg'
+import React from "react"
+import heroStyles from "./hero.module.css"
+import bottomCurve from "../../src/images/curve-bottom.svg"
 import Img from "gatsby-image"
 import { useStaticQuery, Link, graphql } from "gatsby"
+import { useSpring, animated } from "react-spring"
 
-
-export default function Hero(props) {
-    const data = useStaticQuery(
-        graphql`
-          query {
-            file(relativePath: { eq: "boat.png" }) {
-              childImageSharp {
-                fluid (maxWidth: 754) {
-                  ...GatsbyImageSharpFluid
-                  ...GatsbyImageSharpFluidLimitPresentationSize
-                }
-              }
+export default function Hero() {
+  const props = useSpring({
+    height: 540,
+    from: { height: 0 },
+  })
+  const data = useStaticQuery(
+    graphql`
+      query {
+        file(relativePath: { eq: "boat.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 754) {
+              ...GatsbyImageSharpFluid
+              ...GatsbyImageSharpFluidLimitPresentationSize
             }
+          }
         }
-    `)
-    return (
-        <>
-        <div className={heroStyles.hero}>
-          <div className={heroStyles.heroBorder}></div>
-          <div className={heroStyles.imgWrapper}><Img fluid={data.file.childImageSharp.fluid} alt="A boat from above" /></div>
-          <h1>WAVE GOODBYE</h1>
-          <div className={heroStyles.divider}></div>
-            <p>Set sale on the ocean waves and make your dreams come true.</p>
-            <Link className={heroStyles.btn} to="/">BOOK NOW</Link>
+      }
+    `
+  )
+  return (
+    <>
+      <div className={heroStyles.hero}>
+        <div className={heroStyles.heroBorder}></div>
+        <div className={heroStyles.imgWrapper}>
+          <Img
+            fluid={data.file.childImageSharp.fluid}
+            alt="A boat from above"
+          />
         </div>
-      
-      <img className={heroStyles.bottomCurve} src={bottomCurve} alt="Bottom Curve" /> 
+      <div className={heroStyles.headingContainer}>
+        <animated.div style={props}>
+          <h1>WAVE GOODBYE</h1>
+        </animated.div>
+        </div>
+        <div className={heroStyles.divider}></div>
+        <p>Set sale on the ocean waves and make your dreams come true.</p>
+        <Link className={heroStyles.btn} to="/">
+          BOOK NOW
+        </Link>
+      </div>
 
-        </>
-    );
+      <img
+        className={heroStyles.bottomCurve}
+        src={bottomCurve}
+        alt="Bottom Curve"
+      />
+    </>
+  )
 }
-
